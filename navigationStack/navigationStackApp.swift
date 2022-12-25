a1e2c2d04fd230d41ce27a901ca33a9087cecce7
sonerxdev <sonerkaraevli@gmail.com>
2022-12-25 14:02:06 +0300

    Initial Commit

---
+++ navigationStack/navigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationStackApp.swift
+//  navigationStack
+//
+//  Created by Soner Karaevli on 25.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct navigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
